6189c41642371c9547b90f21ed0c5fae83e96480
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2021-07-23 16:08:35 +0530

    db

--- includes/db.php
+++ includes/db.php
@@ -4,11 +4,11 @@ $db['db_host'] = "localhost";
 $db['db_user'] = "u543554482_svps";
 $db['db_pass'] = "svps-admin-2020";
 $db['db_name'] = "u543554482_svps";
-
+// local
 // $db['db_host'] = "localhost";
 // $db['db_user'] = "root";
 // $db['db_pass'] = "";
-// $db['db_name'] = "svps";
+// $db['db_name'] = "svps";  
 
 foreach($db as $key => $value){
 
